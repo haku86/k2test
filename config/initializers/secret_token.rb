e217cb1c1ff090d9ea8b89bf11d61a5b492074ff
Joel <lomyril86@gmail.com>
2013-03-02 14:19:03 +0800

    initiatl commi

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+K2test::Application.config.secret_token = '4fb66be55b8db15ec73f828168a95193ef7f0b3f8a4b31e7a162b59d842ccc5cccb2ea418a4f6d655ecd7d577c1a3c62bb8774128955eb05638728cb50257519'
